d75c37f3dbc97880699d934b3f8e70c899516673
Анастасия Арама <nastyaarama@gmail.com>
2022-06-22 19:28:17 +0500

    Добавьте файлы проекта.

---
+++ Проверка/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Проверка
+{
+    public class Class1
+    {
+        public bool telephone(string tel)
+        {
+            Regex regex = new Regex(@"((^7|^8)\(\d{3}\)\d{3}-\d{2}-\d{2}$)|((^7|^8)\d{10}$)");
+            return regex.IsMatch(tel);
+        }
+
+
+    }
+}
+//public bool email(string em)
+//{
+//    Regex regex = new Regex(@"(^[A-Za-z0-9]{1,})@\w{1,}\.([a-z]{2,})$");
+//    return regex.IsMatch(em);
+//}
+
+//public bool pasport(string ps)
+//{
+//    Regex regex = new Regex(@"(^\d{4})\s(\d{6})$");
+//    return regex.IsMatch(ps);
+//}
+
+//public bool dt(string ps)
+//{
+//    Regex regex = new Regex(@"(^\d{2}.\d{2}.(\d{2}|\d{4}))$");
+//    return regex.IsMatch(ps);
+//}
+
+//public bool fio(string ps)
+//{
+//    Regex regex = new Regex(@"^[А-Я]{1}[а-я]{1,}\s[А-Я]{1}[а-я]{1,}\s[А-Я]{1}[а-я]{1,}$");
+//    return regex.IsMatch(ps);
+//}
---
+++ Проверка/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Проверка
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void test_click(object sender, RoutedEventArgs e)
+        {
+            Class1 cl = new Class1();
+            bool a1 = cl.telephone(ph.Text);
+            if (a1 == true)
+            {
+                MessageBox.Show("Телефон правильный");
+            }
+            else
+            {
+                MessageBox.Show("Телефон не правильный");
+            }
+        }
+
+        private void email_click(object sender, RoutedEventArgs e)
+        {
+            Class1 cl = new Class1();
+            bool a1 = cl.email(email1.Text);
+            if (a1 == true)
+            {
+                MessageBox.Show("Email правильный");
+            }
+            else
+            {
+                MessageBox.Show("Email не правильный");
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Class1 cl = new Class1();
+            bool a1 = cl.pasport(pas.Text);
+            if (a1 == true)
+            {
+                MessageBox.Show("Телефон правильный");
+            }
+            else
+            {
+                MessageBox.Show("Телефон не правильный");
+            }
+        }
+
+        private void st2(object sender, RoutedEventArgs e)
+        {
+            Class1 cl = new Class1();
+            bool a1 = cl.dt(dt1.Text);
+            if (a1 == true)
+            {
+                MessageBox.Show("Телефон правильный");
+            }
+            else
+            {
+                MessageBox.Show("Телефон не правильный");
+            }
+        }
+
+        private void fio2(object sender, RoutedEventArgs e)
+        {
+            Class1 cl = new Class1();
+            bool a1 = cl.fio(fio1.Text);
+            if (a1 == true)
+            {
+                MessageBox.Show("Телефон правильный");
+            }
+            else
+            {
+                MessageBox.Show("Телефон не правильный");
+            }
+        }
+    }
+}
